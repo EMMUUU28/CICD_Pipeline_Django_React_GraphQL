9490ec4ca31dbe96c67fb3cd237e77b1e8792500
EMMUUU28 <francisgudinho1234@gmail.com>
2025-04-04 12:48:09 +0530

    v22

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path,include
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
